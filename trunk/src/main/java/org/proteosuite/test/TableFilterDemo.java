cdaaea3e3ffe3ed73acf9d50d12f1826fa641046
faviel.gonzalez@gmail.com <faviel.gonzalez@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2013-04-02 10:05:09 +0000

    Update to include unmarshall mzq files by elements
    FG

--- trunk/src/main/java/org/proteosuite/test/TableFilterDemo.java
+++ trunk/src/main/java/org/proteosuite/test/TableFilterDemo.java
@@ -5,59 +5,27 @@
 package org.proteosuite.test;
 
 /*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
-
-/*
  * TableFilterDemo.java requires SpringUtilities.java
  */
-
-import org.proteosuite.test.SpringUtilities;
+ 
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.Dimension;
 import java.awt.Component;
-
+ 
 public class TableFilterDemo extends JPanel {
     private boolean DEBUG = false;
     private JTable table;
     private JTextField filterText;
     private JTextField statusText;
     private TableRowSorter<MyTableModel> sorter;
-
+ 
     public TableFilterDemo() {
         super();
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
+ 
         //Create a table with a sorter.
         MyTableModel model = new MyTableModel();
         sorter = new TableRowSorter<MyTableModel>(model);
@@ -65,11 +33,11 @@ public class TableFilterDemo extends JPanel {
         table.setRowSorter(sorter);
         table.setPreferredScrollableViewportSize(new Dimension(500, 70));
         table.setFillsViewportHeight(true);
-
+ 
         //For the purposes of this example, better to have a single
         //selection.
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
+ 
         //When selection changes, provide user with row numbers for
         //both view and model.
         table.getSelectionModel().addListSelectionListener(
@@ -80,24 +48,24 @@ public class TableFilterDemo extends JPanel {
                             //Selection got filtered away.
                             statusText.setText("");
                         } else {
-                            int modelRow = 
+                            int modelRow =
                                 table.convertRowIndexToModel(viewRow);
                             statusText.setText(
                                 String.format("Selected Row in view: %d. " +
-                                    "Selected Row in model: %d.", 
+                                    "Selected Row in model: %d.",
                                     viewRow, modelRow));
                         }
                     }
                 }
         );
-
-
+ 
+ 
         //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
-
+ 
         //Add the scroll pane to this panel.
         add(scrollPane);
-
+ 
         //Create a separate form for filterText and statusText
         JPanel form = new JPanel(new SpringLayout());
         JLabel l1 = new JLabel("Filter Text:", SwingConstants.TRAILING);
@@ -126,8 +94,8 @@ public class TableFilterDemo extends JPanel {
         SpringUtilities.makeCompactGrid(form, 2, 2, 6, 6, 6, 6);
         add(form);
     }
-
-    /** 
+ 
+    /**
      * Update the row filter regular expression from the expression in
      * the text box.
      */
@@ -141,10 +109,10 @@ public class TableFilterDemo extends JPanel {
         }
         sorter.setRowFilter(rf);
     }
-
-
-
-
+ 
+ 
+ 
+ 
     class MyTableModel extends AbstractTableModel {
         private String[] columnNames = {"First Name",
                                         "Last Name",
@@ -152,34 +120,34 @@ public class TableFilterDemo extends JPanel {
                                         "# of Years",
                                         "Vegetarian"};
         private Object[][] data = {
-	    {"Kathy", "Smith",
-	     "Snowboarding", new Integer(5), new Boolean(false)},
-	    {"John", "Doe",
-	     "Rowing", new Integer(3), new Boolean(true)},
-	    {"Sue", "Black",
-	     "Knitting", new Integer(2), new Boolean(false)},
-	    {"Jane", "White",
-	     "Speed reading", new Integer(20), new Boolean(true)},
-	    {"Joe", "Brown",
-	     "Pool", new Integer(10), new Boolean(false)}
+        {"Kathy", "Smith",
+         "Snowboarding", new Integer(5), new Boolean(false)},
+        {"John", "Doe",
+         "Rowing", new Integer(3), new Boolean(true)},
+        {"Sue", "Black",
+         "Knitting", new Integer(2), new Boolean(false)},
+        {"Jane", "White",
+         "Speed reading", new Integer(20), new Boolean(true)},
+        {"Joe", "Brown",
+         "Pool", new Integer(10), new Boolean(false)}
         };
-
+ 
         public int getColumnCount() {
             return columnNames.length;
         }
-
+ 
         public int getRowCount() {
             return data.length;
         }
-
+ 
         public String getColumnName(int col) {
             return columnNames[col];
         }
-
+ 
         public Object getValueAt(int row, int col) {
             return data[row][col];
         }
-
+ 
         /*
          * JTable uses this method to determine the default renderer/
          * editor for each cell.  If we didn't implement this method,
@@ -189,7 +157,7 @@ public class TableFilterDemo extends JPanel {
         public Class getColumnClass(int c) {
             return getValueAt(0, c).getClass();
         }
-
+ 
         /*
          * Don't need to implement this method unless your table's
          * editable.
@@ -203,7 +171,7 @@ public class TableFilterDemo extends JPanel {
                 return true;
             }
         }
-
+ 
         /*
          * Don't need to implement this method unless your table's
          * data can change.
@@ -215,20 +183,20 @@ public class TableFilterDemo extends JPanel {
                                    + " (an instance of "
                                    + value.getClass() + ")");
             }
-
+ 
             data[row][col] = value;
             fireTableCellUpdated(row, col);
-
+ 
             if (DEBUG) {
                 System.out.println("New value of data:");
                 printDebugData();
             }
         }
-
+ 
         private void printDebugData() {
             int numRows = getRowCount();
             int numCols = getColumnCount();
-
+ 
             for (int i=0; i < numRows; i++) {
                 System.out.print("    row " + i + ":");
                 for (int j=0; j < numCols; j++) {
@@ -239,7 +207,7 @@ public class TableFilterDemo extends JPanel {
             System.out.println("--------------------------");
         }
     }
-
+ 
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -249,17 +217,17 @@ public class TableFilterDemo extends JPanel {
         //Create and set up the window.
         JFrame frame = new JFrame("TableFilterDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+ 
         //Create and set up the content pane.
         TableFilterDemo newContentPane = new TableFilterDemo();
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
-
+ 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
-
+ 
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
---
+++ trunk/src/main/java/org/proteosuite/utils/ExcelExporter.java
@@ -0,0 +1,64 @@
+/*
+ * --------------------------------------------------------------------------
+ * ExcelExporter.java
+ * --------------------------------------------------------------------------
+ * Description:       Convert a jtable to a spreadsheet
+ * Developer:         Faviel Gonzalez
+ * Created:           11 March 2013
+ * Read our documentation under our Google SVN repository
+ * SVN: http://code.google.com/p/proteo-suite/
+ * Project Website: http://www.proteosuite.org/
+ * --------------------------------------------------------------------------
+ */
+package org.proteosuite.utils;
+
+import java.io.File;
+import javax.swing.*;
+import javax.swing.table.*;
+import jxl.*;
+import jxl.write.*;
+
+/**
+ * This class allows to export any table into a spreadsheet
+ * @param table - Table to export
+ * @param file - File name in which the data will be exported
+ * @param bAll - Export all columns
+ * @param iExclude - Exclude column
+ * @author faviel
+ */
+public class ExcelExporter {
+
+    public void fillData(JTable table, File file, boolean bAll, int iExclude) {
+        try {
+            WritableWorkbook wbk = Workbook.createWorkbook(file);
+            WritableSheet sheet1 = wbk.createSheet("Results", 0);
+            TableModel model = table.getModel();
+
+            for (int iI = 0; iI < model.getColumnCount(); iI++) {
+                if (bAll){
+                    Label column = new Label(iI, 0, model.getColumnName(iI));
+                    sheet1.addCell(column);                    
+                }else if (iI!=iExclude){
+                    Label column = new Label(iI, 0, model.getColumnName(iI));
+                    sheet1.addCell(column);
+                }
+            }
+            for (int iI = 0; iI < model.getRowCount(); iI++) {
+                for (int iJ = 0; iJ < model.getColumnCount(); iJ++) {
+                    if (bAll){                    
+                        Label row = new Label(iJ, iI + 1, model.getValueAt(iI, iJ).toString());
+                        sheet1.addCell(row);
+                    }else if (iJ!=iExclude){
+                        Label row = new Label(iJ, iI + 1, model.getValueAt(iI, iJ).toString());
+                        sheet1.addCell(row);
+                    }
+                }
+            }
+            wbk.write();
+            wbk.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
