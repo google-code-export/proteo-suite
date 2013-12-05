7c489765899c60834958c0ae2f52f928c4f3c953
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2013-12-05 23:05:01 +0000

    * Fixed Feature viewer not showing * Removed instance of GroupLayout in main design * Removed GroupLayout in every part of the properties tabs! * Removed redundant JPanels to give a tidier feel

--- trunk/src/main/java/org/proteosuite/gui/HeaderPanel.java
+++ trunk/src/main/java/org/proteosuite/gui/HeaderPanel.java
@@ -1,10 +1,8 @@
-package org.proteosuite.gui.toolbars;
+package org.proteosuite.gui;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.util.List;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
@@ -12,24 +10,15 @@ import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.JToolBar;
-import javax.swing.border.EtchedBorder;
 
 import org.proteosuite.ProteoSuiteView;
-import org.proteosuite.gui.IdentParamsView;
-import org.proteosuite.gui.TabbedChartViewer;
-import org.proteosuite.gui.TabbedLog;
-import org.proteosuite.gui.TabbedProperties;
+import org.proteosuite.gui.toolbars.MasterToolBar;
 
 import uk.ac.ebi.jmzidml.xml.io.MzIdentMLUnmarshaller;
 
-/**
- * 
- * @author Andrew Collins
- */
-public class MasterToolBar extends JPanel {
+public class HeaderPanel extends JPanel {
 
-	public MasterToolBar(ProteoSuiteView proteoSuiteView,
+	public HeaderPanel (ProteoSuiteView proteoSuiteView,
 			JMenuItem jmCloseProject, JMenuItem jmSaveProject,
 			TabbedProperties jtpProperties, TabbedLog jtpLog,
 			TabbedChartViewer jtpViewer, 
@@ -46,32 +35,24 @@ public class MasterToolBar extends JPanel {
 			JTable jtMzIDProtGroup,
 			JComboBox<String> jcbPSM, JLabel jlRawFilesStatus,
 			IdentParamsView identParamsExecute,
-			List<MzIdentMLUnmarshaller> aMzIDUnmarshaller) {
-		JToolBar projectToolBar = new ProjectToolBar(proteoSuiteView,
-				jmCloseProject, jmSaveProject, jtpProperties, jtpLog,
-				jtpViewer, 
+			List<MzIdentMLUnmarshaller> aMzIDUnmarshaller)
+	{
+		final JPanel jpToolBar = new MasterToolBar(proteoSuiteView, jmCloseProject,
+				jmSaveProject, jtpProperties, jtpLog, jtpViewer,
 				jbSaveProject, jcbTechnique, jepMGFView, jepMZQView,
 				jepMascotXMLView, jepMzIDView, jepMzMLView, jlFileNameMzMLText,
 				jlFileNameMzIDText, jlIdentFilesStatus, jlFileNameMzQText,
 				jtRawFiles, jtIdentFiles, jtQuantFiles, jtRawData, jtMzML,
 				jtMGF, jtMzId, jtMascotXMLView, jtPeptideQuant, jtProteinQuant,
 				jtFeatureQuant, jcbOutputFormat, jlFileNameMGFText,
-				jtMzIDProtGroup, jcbPSM, jlRawFilesStatus);
-		JToolBar manipulationToolBar = new ManipulationToolBar();
-		JToolBar analysisToolBar = new AnalysisToolBar(proteoSuiteView,
-				jmCloseProject, jmSaveProject, jtpProperties, jtpLog,
-				jbSaveProject, jcbTechnique, jepMZQView,
-				jepMzIDView, jlFileNameMzIDText, jlIdentFilesStatus,
-				jlFileNameMzQText, jtRawFiles, jtIdentFiles, jtQuantFiles,
-				jtMzId, jtPeptideQuant, jtProteinQuant, jtFeatureQuant,
-				jcbOutputFormat, jtMzIDProtGroup,  jcbPSM,
-				jlRawFilesStatus, identParamsExecute, aMzIDUnmarshaller);
+				jtMzIDProtGroup, jcbPSM, jlRawFilesStatus,
+				identParamsExecute, aMzIDUnmarshaller);
 
-		setPreferredSize(new Dimension(933, 32));
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		final JPanel jpProjectStatus = new ProjectStatus(jcbTechnique,
+				jcbOutputFormat, jlRawFilesStatus, jlIdentFilesStatus);
 		
-		add(projectToolBar);
-		add(manipulationToolBar);
-		add(analysisToolBar);
+		setLayout(new BorderLayout());
+		add(jpToolBar, BorderLayout.PAGE_START);
+		add(jpProjectStatus, BorderLayout.PAGE_END);
 	}
 }
--- trunk/src/main/java/org/proteosuite/gui/MainPanel.java
+++ trunk/src/main/java/org/proteosuite/gui/MainPanel.java
@@ -1,10 +1,14 @@
 package org.proteosuite.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 
 /**
  * 
@@ -14,23 +18,21 @@ public class MainPanel extends JPanel {
 	private JSplitPane jspMainPanelView = new JSplitPane();
 	private JSplitPane jspViewerAndProperties = new JSplitPane();
 
-	public MainPanel(JTable jtQuantFiles, JTable jtIdentFiles, JTable jtRawFiles, TabbedChartViewer jtpViewer, 
-			TabbedLog jtpLog, TabbedProperties jtpProperties) {
-		JPanel jpLeftPanelView = new LeftPanelView(jtQuantFiles, jtIdentFiles,
-				jtRawFiles);
-		JPanel jpLeftViewer = new LeftViewer(jtpViewer, jtpLog);
-		JPanel jpPropertiesBox = new PropertiesView(jtpProperties);
-		
-		
+	public MainPanel(JTable jtQuantFiles, JTable jtIdentFiles,
+			JTable jtRawFiles, TabbedChartViewer jtpViewer, TabbedLog jtpLog,
+			TabbedProperties jtpProperties) {
 		jspViewerAndProperties.setDividerLocation(380);
-		jspViewerAndProperties.setDividerSize(3);
-		jspViewerAndProperties.setLeftComponent(jpLeftViewer);
-		jspViewerAndProperties.setRightComponent(jpPropertiesBox);
-		
+		jspViewerAndProperties.setDividerSize(5);
+		jspViewerAndProperties.setLeftComponent(getLeftViewer(jtpViewer, jtpLog));
+		jspViewerAndProperties.setRightComponent(getPropertiesView(jtpProperties));
+		jspViewerAndProperties.setBorder(null);
+
 		jspMainPanelView.setDividerLocation(280);
-		jspMainPanelView.setDividerSize(3);
-		jspMainPanelView.setLeftComponent(jpLeftPanelView);
+		jspMainPanelView.setDividerSize(5);
+		jspMainPanelView.setLeftComponent(new LeftPanelView(jtQuantFiles, jtIdentFiles,
+				jtRawFiles));
 		jspMainPanelView.setRightComponent(jspViewerAndProperties);
+		jspMainPanelView.setBorder(null);
 
 		// Viewer Width (Viewer)
 		jspViewerAndProperties.setDividerLocation(600);
@@ -47,5 +49,49 @@ public class MainPanel extends JPanel {
 
 	public JSplitPane getViewerAndProperties() {
 		return jspViewerAndProperties;
+	}
+
+	public JPanel getLeftViewer(TabbedChartViewer jtpViewer, TabbedLog jtpLog) {
+		JLabel jlViewer = new JLabel("Viewer");
+		jlViewer.setFont(new Font("Verdana", 1, 14)); // NOI18N
+		jlViewer.setForeground(new Color(102, 102, 102));
+		jlViewer.setBorder(new EmptyBorder(5, 10, 5, 0));
+
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new BorderLayout());
+		jPanel.add(jlViewer, BorderLayout.PAGE_START);
+		jPanel.add(getLeftViewerDetails(jtpViewer, jtpLog), BorderLayout.CENTER);
+
+		return jPanel;
+	}
+
+	private JSplitPane getLeftViewerDetails(TabbedChartViewer jtpViewer,
+			TabbedLog jtpLog) {
+
+		final JSplitPane jspLeftViewerDetails = new JSplitPane();
+		jspLeftViewerDetails.setDividerLocation(350);
+		jspLeftViewerDetails.setDividerSize(2);
+		jspLeftViewerDetails.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		jspLeftViewerDetails.setTopComponent(jtpViewer);
+		jspLeftViewerDetails.setRightComponent(jtpLog);
+
+		// Viewer Height (Viewer)
+		jspLeftViewerDetails.setDividerLocation(480);
+
+		return jspLeftViewerDetails;
+	}
+
+	public JPanel getPropertiesView(TabbedProperties jtpProperties) {
+		JLabel jlProperties = new JLabel("Data and Metadata");
+		jlProperties.setFont(new Font("Verdana", 1, 14)); // NOI18N
+		jlProperties.setForeground(new Color(102, 102, 102));
+		jlProperties.setBorder(new EmptyBorder(5, 10, 5, 0));
+
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new BorderLayout());
+		jPanel.add(jlProperties, BorderLayout.PAGE_START);
+		jPanel.add(jtpProperties, BorderLayout.CENTER);
+
+		return jPanel;
 	}
 }
