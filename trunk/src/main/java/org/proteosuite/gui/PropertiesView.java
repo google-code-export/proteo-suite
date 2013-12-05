9ded0b7127f279d7a848c06f0b14a5cdc27e21f8
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2013-12-05 17:00:25 +0000

    * Removed redundant interface objects.

--- trunk/src/main/java/org/proteosuite/gui/PropertiesView.java
+++ trunk/src/main/java/org/proteosuite/gui/PropertiesView.java
@@ -2,12 +2,10 @@ package org.proteosuite.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -16,45 +14,14 @@ import javax.swing.border.EmptyBorder;
  */
 public class PropertiesView extends JPanel {
 
-	public PropertiesView(TabbedProperties jtpProperties) {
-
-		JPanel jpPropetiesTab = getPropertiesTab(jtpProperties);
-		JPanel jpProperties = getPropertiesPanel();
-
-		final JSplitPane jspProperties = new JSplitPane();
-		jspProperties.setDividerLocation(20);
-		jspProperties.setDividerSize(1);
-		jspProperties.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		jspProperties.setTopComponent(jpProperties);
-		jspProperties.setRightComponent(jpPropetiesTab);
-
-		setLayout(new BorderLayout());
-		add(jspProperties);
-	}
-
-	private JPanel getPropertiesPanel() {
+	public PropertiesView(TabbedProperties jtpProperties) {		
 		JLabel jlProperties = new JLabel("Data and Metadata");
 		jlProperties.setFont(new Font("Verdana", 1, 14)); // NOI18N
 		jlProperties.setForeground(new Color(102, 102, 102));
+		jlProperties.setBorder(new EmptyBorder(5, 10, 5, 0));
 
-		JPanel jpProperties = new JPanel();
-
-		jpProperties.setMinimumSize(new Dimension(302, 25));
-		jpProperties.setPreferredSize(new Dimension(313, 25));
-		jpProperties.setLayout(new BorderLayout());
-		jpProperties.add(jlProperties);
-		jlProperties.setBorder(new EmptyBorder(0, 10, 0, 0));
-
-		return jpProperties;
-	}
-
-	private JPanel getPropertiesTab(TabbedProperties jtpProperties) {
-
-		JPanel jpPropetiesTab = new JPanel();
-		
-		jpPropetiesTab.setLayout(new BorderLayout());
-		jpPropetiesTab.add(jtpProperties);
-
-		return jpPropetiesTab;
+		setLayout(new BorderLayout());
+		add(jlProperties, BorderLayout.PAGE_START);
+		add(jtpProperties, BorderLayout.CENTER);
 	}
 }
