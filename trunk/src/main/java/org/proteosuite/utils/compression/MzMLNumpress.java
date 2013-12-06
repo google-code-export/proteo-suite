da27a2618f68e810ba9c7decc522af18bfb6b543
simon.richard.perkins@gmail.com <simon.richard.perkins@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2013-12-06 16:19:45 +0000

    -Removed JAR files from lib directory which are maven resolved. -Removed commons-lang dependency as it didn't seem to be used (could be wrong!) -Updates to compression code.

--- trunk/src/main/java/org/proteosuite/utils/compression/MzMLNumpress.java
+++ trunk/src/main/java/org/proteosuite/utils/compression/MzMLNumpress.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.List;
 import uk.ac.ebi.jmzml.model.mzml.BinaryDataArray;
 import uk.ac.ebi.jmzml.model.mzml.BinaryDataArrayList;
-import uk.ac.ebi.jmzml.model.mzml.CV;
 import uk.ac.ebi.jmzml.model.mzml.CVParam;
 import uk.ac.ebi.jmzml.model.mzml.params.BinaryDataArrayCVParam;
 
@@ -27,6 +26,8 @@ import uk.ac.ebi.jmzml.model.mzml.params.BinaryDataArrayCVParam;
  * @author SPerkins
  */
 public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
+    private final String LINEAR_COMPRESSION_ACCESSION = "MS:1002312";
+    private final String PIC_COMPRESSION_ACCESSION = "MS:1002313";
     
     public MzMLNumpress() {
         super();
@@ -73,7 +74,13 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
     public static double[] doublesFromNumbers(final Number[] numbers) {
         double[] doubles = new double[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            doubles[i] = (Double) numbers[i];
+            if (numbers[i] instanceof Integer) {
+                doubles[i] = (Integer) numbers[i];
+            } else if (numbers[i] instanceof Float) {
+                doubles[i] = (Float) numbers[i];
+            } else if (numbers[i] instanceof Double) {
+                doubles[i] = (Double) numbers[i];
+            }           
         }
 
         return doubles;
@@ -92,12 +99,14 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
                     if (cvp.getAccession().equals("MS:1000514")) {
                         if (bda.getEncodedLength() > 0) {
                             mzNumbers = bda.getBinaryDataAsNumberArray();
+                            break;
                         }
                     }
                     
                     if (cvp.getAccession().equals("MS:1000515")) {
                         if (bda.getEncodedLength() > 0) {
                             intenNumbers = bda.getBinaryDataAsNumberArray();
+                            break;
                         }
                     }
                 }
@@ -109,12 +118,9 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
 
             if (mzNumbers != null && mzNumbers.length > 0
                     && intenNumbers != null
-                    && intenNumbers.length == mzNumbers.length) {
-                CV cv = new CV();
-                cv.setFullName("PSI-MS");
-                cv.setId("MS");
+                    && intenNumbers.length == mzNumbers.length) {                
 
-                // Numpress code.               
+                // Numpress code.
                 byte[] encodedMzNumbers = new byte[8 + (mzNumbers.length * 5)];
                 int encodedBytes = MSNumpress.encodeLinear(doublesFromNumbers(mzNumbers), mzNumbers.length, encodedMzNumbers, 
                         MSNumpress.optimalLinearFixedPoint(doublesFromNumbers(mzNumbers), mzNumbers.length));
@@ -124,10 +130,10 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
                 bdaW.setEncodedLength(mzNumbers.length);
                 
                 // Set a CVParam for the numpress linear compression.
-                CVParam compressionParam = new BinaryDataArrayCVParam();
-                compressionParam.setAccession("MS:1000574");
-                compressionParam.setName("numpress linear compression");
-                bdaW.getCvParam().add(compressionParam);
+                CVParam linearCompressionParam = new BinaryDataArrayCVParam();
+                linearCompressionParam.setAccession(LINEAR_COMPRESSION_ACCESSION);
+                linearCompressionParam.setName("numpress linear compression");
+                bdaW.getCvParam().add(linearCompressionParam);
 
                 CVParam cvParam1 = new BinaryDataArrayCVParam();
                 cvParam1.setUnitCvRef("MS");
@@ -136,7 +142,7 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
                 cvParam1.setName("m/z array");
                 cvParam1.setAccession("MS:1000514");
                 cvParam1.setCvRef("MS");
-                bdaW.getCvParam().add(cvParam1);                
+                bdaW.getCvParam().add(cvParam1);
                 
                 byte[] encodedIntensityNumbers = new byte[(intenNumbers.length * 5)];
                 encodedBytes = MSNumpress.encodePic(doublesFromNumbers(intenNumbers), intenNumbers.length, encodedIntensityNumbers);
@@ -144,7 +150,12 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
                 bda2W.setBinary(Arrays.copyOf(encodedIntensityNumbers, encodedBytes));
                 bda2W.setEncodedLength(intenNumbers.length);
                 
-                bda2W.getCvParam().add(compressionParam);
+                // Set a CVParam for the numpress pic compression.
+                CVParam picCompressionParam = new BinaryDataArrayCVParam();
+                picCompressionParam.setAccession(PIC_COMPRESSION_ACCESSION);
+                picCompressionParam.setName("numpress pic compression");
+                
+                bda2W.getCvParam().add(picCompressionParam);
 
                 CVParam cvParam2 = new BinaryDataArrayCVParam();
                 cvParam2.setUnitCvRef("MS");
@@ -153,16 +164,67 @@ public class MzMLNumpress extends MzMLCompressorBase implements MzMLCompressor {
                 cvParam2.setName("intensity array");
                 cvParam2.setAccession("MS:1000515");
                 cvParam2.setCvRef("MS");
-                bda2W.getCvParam().add(cvParam2);                
-            }
-            
-            destination.getBinaryDataArray().add(bdaW);
-            destination.getBinaryDataArray().add(bda2W);
-            destination.setCount(2);
+                bda2W.getCvParam().add(cvParam2);
+                
+                destination.getBinaryDataArray().add(bdaW);
+                destination.getBinaryDataArray().add(bda2W);
+                destination.setCount(2);
+            } else {
+                destination.setCount(0);
+            }           
     }
     
     @Override
-    protected void decompressBinaryDataArrayList(BinaryDataArrayList source, BinaryDataArrayList destination) {
+    protected void decompressBinaryDataArrayList(final BinaryDataArrayList source, BinaryDataArrayList destination) {
+        for (BinaryDataArray dataArray : source.getBinaryDataArray()) {
+            List<CVParam> paramList = dataArray.getCvParam();
+            byte[] binary = dataArray.getBinary();
+            double[] decodedDoubles = new double[dataArray.getEncodedLength()];
+            BinaryDataArray outDataArray = new BinaryDataArray();
+            for (int i = 0; i < paramList.size(); i++) {
+                CVParam param = paramList.get(i);
+                try {
+                    if (param.getAccession().equals(LINEAR_COMPRESSION_ACCESSION)) {
+                        int decodedDoubleCount = MSNumpress.decodeLinear(binary, binary.length, decodedDoubles);
+                        if (decodedDoubleCount != dataArray.getEncodedLength()) {
+                            throw new NumpressDecodeException();
+                        }
+                        
+                        paramList.remove(i);
+                        break;                      
+                    } else if (param.getAccession().equals(PIC_COMPRESSION_ACCESSION)) {
+                        int decodedDoubleCount = MSNumpress.decodePic(binary, binary.length, decodedDoubles);
+                        if (decodedDoubleCount != dataArray.getEncodedLength()) {
+                            throw new NumpressDecodeException();
+                        }
+                        
+                        paramList.remove(i);
+                        break;
+                        
+                    } else if (i == (paramList.size() - 1)) {
+                        throw new NumpressDecodeException("Data is not numpress encoded.");
+                    }                   
+                } catch (NumpressDecodeException n) {
+                    System.out.println("Problem decoding numpress data. Data has not been decoded.");                    
+                }               
+            } 
+            
+            outDataArray.getCvParam().addAll(paramList);
+            outDataArray.set64BitFloatArrayAsBinaryData(decodedDoubles, true, getCV());
+            outDataArray.setEncodedLength(decodedDoubles.length);
+            destination.getBinaryDataArray().add(outDataArray);
+        }
+        
+        destination.setCount(destination.getBinaryDataArray().size());
+    }
     
+    public static class NumpressDecodeException extends Exception {
+        public NumpressDecodeException() {
+            this("Data length decoded did not match data length decoded.");
+        }
+        
+        public NumpressDecodeException(String message) {
+            super(message);
+        }
     }
 }
--- trunk/src/main/java/org/proteosuite/utils/compression/SpectrumTrimmer.java
+++ trunk/src/main/java/org/proteosuite/utils/compression/SpectrumTrimmer.java
@@ -2,33 +2,33 @@
  * --------------------------------------------------------------------------
  * SpectrumTrimmer.java
  * --------------------------------------------------------------------------
- * Description:       Utility to trim zero intensity pairs.
- * Developer:         SPerkins
- * Created:           26 November 2013
- * Read our documentation file under our Google SVN repository
- * SVN: http://code.google.com/p/proteo-suite/
- * Project Website: http://www.proteosuite.org/
+ * Description: Utility to trim zero intensity pairs. Developer: SPerkins
+ * Created: 26 November 2013 Read our documentation file under our Google SVN
+ * repository SVN: http://code.google.com/p/proteo-suite/ Project Website:
+ * http://www.proteosuite.org/
  * --------------------------------------------------------------------------
  */
-
 package org.proteosuite.utils.compression;
 
 import java.util.Arrays;
 
 /**
  * Class for trimming spectra of zero intensity pairs.
+ *
  * @author SPerkins
  */
 public class SpectrumTrimmer {
+
     private Number[] mzValues;
     private Number[] intensityValues;
     private int trimmedSize;
     private Number[] trimmedMzValues;
     private Number[] trimmedIntensityValues;
     private boolean trimmed = false;
-    
+
     /**
      * Constructor for SpectrumTrimmer.
+     *
      * @param mzValues M/Z value data.
      * @param intensities Intensity value data.
      */
@@ -38,48 +38,71 @@ public class SpectrumTrimmer {
         trimmedMzValues = new Number[mzValues.length];
         trimmedIntensityValues = new Number[intensities.length];
     }
-    
+
     /**
      * Gets the trimmed M/Z value data.
+     *
      * @return Trimmed M/Z value data.
      */
     public Number[] getMzValues() {
         if (!trimmed) {
             trimZeros();
         }
-        
+
         return trimmedMzValues;
     }
-    
+
     /**
      * Gets the trimmed intensity value data.
+     *
      * @return Trimmed intensity value data.
      */
     public Number[] getIntensityValues() {
         if (!trimmed) {
             trimZeros();
         }
-        
+
         return trimmedIntensityValues;
     }
-    
+
     /**
      * Performs the trimming operation.
      */
     private void trimZeros() {
         trimmedSize = 0;
         for (int i = 0; i < mzValues.length; i++) {
-            if (((Double)intensityValues[i] > 0.0d)) {
-                trimmedMzValues[trimmedSize] = mzValues[i];
-                trimmedIntensityValues[trimmedSize] = intensityValues[i];
-                trimmedSize++;
+            if (intensityValues[i] instanceof Double) {
+                if (((Double) intensityValues[i] > 0.0d)) {
+                    trimmedMzValues[trimmedSize] = mzValues[i];
+                    trimmedIntensityValues[trimmedSize] = intensityValues[i];
+                    trimmedSize++;
+                }
+            } else if (intensityValues[i] instanceof Float) {
+                if (((Float) intensityValues[i] > 0.0f)) {
+                    trimmedMzValues[trimmedSize] = mzValues[i];
+                    trimmedIntensityValues[trimmedSize] = intensityValues[i];
+                    trimmedSize++;
+                }
+            } else if (intensityValues[i] instanceof Integer) {
+                if (((Integer) intensityValues[i] > 0)) {
+                    trimmedMzValues[trimmedSize] = mzValues[i];
+                    trimmedIntensityValues[trimmedSize] = intensityValues[i];
+                    trimmedSize++;
+                }
+            } else if (intensityValues[i] instanceof Long) {
+                if (((Long) intensityValues[i] > 0L)) {
+                    trimmedMzValues[trimmedSize] = mzValues[i];
+                    trimmedIntensityValues[trimmedSize] = intensityValues[i];
+                    trimmedSize++;
+                }
             }
+
         }
-        
+
         trimmedMzValues = Arrays.copyOf(trimmedMzValues, trimmedSize);
         trimmedIntensityValues = Arrays.copyOf(trimmedIntensityValues, trimmedSize);
         mzValues = null;
         intensityValues = null;
         trimmed = true;
-    }  
+    }
 }
