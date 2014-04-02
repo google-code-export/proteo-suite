21dac5213a04a173e42e064049fa8c7ce502f2ad
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2014-04-02 16:21:20 +0000

    * Fixed raw file "Delete Selected" should not be enabled unless file selected * Fixed continue is allowed when no raw files are loaded * Fixed clear all should be disabled when no files present * Fixed clicking cancel when loading raw data initiates Raw Data throbber

---
+++ src/main/java/org/proteosuite/gui/listener/RawDataTableListener.java
@@ -0,0 +1,76 @@
+package org.proteosuite.gui.listener;
+
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.JButton;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+
+import org.proteosuite.gui.tables.RawDataAndMultiplexingTable;
+
+public class RawDataTableListener implements ListSelectionListener,
+		TableModelListener {
+	private final RawDataAndMultiplexingTable rawDataTable;
+	private final JButton deleteSelectedButton;
+	private final JButton clearAllButton;
+	private final JButton continueButton;
+	private final JButton restartButton;
+
+	public RawDataTableListener(RawDataAndMultiplexingTable rawDataTable,
+			JButton deleteSelectedButton) {
+		this.rawDataTable = rawDataTable;
+		this.deleteSelectedButton = deleteSelectedButton;
+
+		clearAllButton = null;
+		continueButton = null;
+		restartButton = null;
+	}
+
+	public RawDataTableListener(JButton deleteSelectedButton,
+			JButton clearAllButton, JButton continueButton,
+			JButton restartButton) {
+		this.deleteSelectedButton = deleteSelectedButton;
+		this.clearAllButton = clearAllButton;
+		this.continueButton = continueButton;
+		this.restartButton = restartButton;
+
+		rawDataTable = null;
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting())
+			return;
+
+		DefaultListSelectionModel model = (DefaultListSelectionModel) e
+				.getSource();
+
+		if (model.getAnchorSelectionIndex() == -1)
+			deleteSelectedButton.setEnabled(false);
+		else
+			deleteSelectedButton.setEnabled(true);
+
+		if (rawDataTable.getRowCount() == 0)
+			deleteSelectedButton.setEnabled(false);
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent e) {
+		switch (e.getType()) {
+		case TableModelEvent.DELETE:
+			if (rawDataTable.getRowCount() == 0) {
+				clearAllButton.setEnabled(false);
+				deleteSelectedButton.setEnabled(false);
+				continueButton.setEnabled(false);
+			}
+			break;
+		case TableModelEvent.INSERT:
+			continueButton.setEnabled(true);
+			clearAllButton.setEnabled(true);
+			restartButton.setEnabled(true);
+			break;
+		}
+	}
+
+}
