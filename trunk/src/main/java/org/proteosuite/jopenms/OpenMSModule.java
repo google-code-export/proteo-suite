80255ddaea2d00e0a38c3e5520b957e156d91daa
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2014-04-07 14:03:17 +0000

    * Rest of the general tidying!

--- trunk/src/main/java/org/proteosuite/jopenms/OpenMSModule.java
+++ trunk/src/main/java/org/proteosuite/jopenms/OpenMSModule.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.proteosuite.jopenms;
 
 import java.io.File;
--- trunk/src/main/java/org/proteosuite/jopenms/command/JOpenMS.java
+++ trunk/src/main/java/org/proteosuite/jopenms/command/JOpenMS.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.proteosuite.jopenms.command;
 
 import java.io.*;
