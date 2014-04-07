cb0886aea50aff883581da3aaff9ecd8f33f0e7e
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2014-04-07 13:47:39 +0000

    * Removed Excel export and various unused files

--- src/main/java/org/proteosuite/gui/listener/AddRawDataListener.java
+++ src/main/java/org/proteosuite/gui/listener/AddRawDataListener.java
@@ -14,41 +14,40 @@ import org.proteosuite.model.AnalyseData;
 import org.proteosuite.model.RawMzMLFile;
 
 /**
- *
+ * 
  * @author SPerkins
  */
 public class AddRawDataListener implements ActionListener {
-    private final RawDataAndMultiplexingStep step;
-    
-    public AddRawDataListener(RawDataAndMultiplexingStep step) {
-        this.step = step;    
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        AnalyseData data = AnalyseData.getInstance();
-        JFileChooser chooser = new JFileChooser(ProteoSuiteView.sPreviousLocation);
-        FileNameExtensionFilter mzML_filter = new FileNameExtensionFilter(
-        "mzML Data Files", "mzML", "mzML");
-        chooser.setMultiSelectionEnabled(true);
-        chooser.setFileFilter(mzML_filter);
-        chooser.setAcceptAllFileFilterUsed(false);
-        int returnVal = chooser.showOpenDialog(step);
-        if (returnVal != JFileChooser.APPROVE_OPTION)
-        	return;
-        	
-        	
-            File[] files = chooser.getSelectedFiles(); 
-            if (!files[0].getParent().equals(ProteoSuiteView.sPreviousLocation)) {
-                ProteoSuiteView.sPreviousLocation = files[0].getParent();
-            }
-            
-            for (File f : files) {
-                data.addRawDataFile(new RawMzMLFile(f));               
-            }
-        
-        
-        AnalyseDynamicTab.getInstance().getAnalyseStatusPanel().setRawDataProcessing();
-        step.refreshFromData();
-    }
+	private final RawDataAndMultiplexingStep step;
+
+	public AddRawDataListener(RawDataAndMultiplexingStep step) {
+		this.step = step;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		AnalyseData data = AnalyseData.getInstance();
+		JFileChooser chooser = new JFileChooser(
+				ProteoSuiteView.sPreviousLocation);
+		chooser.setMultiSelectionEnabled(true);
+		chooser.setFileFilter(new FileNameExtensionFilter("mzML Data Files",
+				"mzML"));
+		chooser.setAcceptAllFileFilterUsed(false);
+		int returnVal = chooser.showOpenDialog(step);
+		if (returnVal != JFileChooser.APPROVE_OPTION)
+			return;
+
+		File[] files = chooser.getSelectedFiles();
+		if (!files[0].getParent().equals(ProteoSuiteView.sPreviousLocation)) {
+			ProteoSuiteView.sPreviousLocation = files[0].getParent();
+		}
+
+		for (File f : files) {
+			data.addRawDataFile(new RawMzMLFile(f));
+		}
+
+		AnalyseDynamicTab.getInstance().getAnalyseStatusPanel()
+				.setRawDataProcessing();
+		step.refreshFromData();
+	}
 }
