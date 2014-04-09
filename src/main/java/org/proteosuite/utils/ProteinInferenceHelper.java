c4f19e1f116488c73f4792424d0682f79ba64528
simon.richard.perkins@googlemail.com <simon.richard.perkins@googlemail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2014-04-09 15:10:41 +0000

    -Updated mzidLib maven dependency to 1.6.5 -Added FastUtil as a top level dependency, as for some reason it was not being added when a secondary dependency through jmzml. -Set the retention time correction of mzid files to happen in the background.

--- src/main/java/org/proteosuite/utils/ProteinInferenceHelper.java
+++ src/main/java/org/proteosuite/utils/ProteinInferenceHelper.java
@@ -12,88 +12,89 @@ import org.proteosuite.model.Task;
 import uk.ac.man.mzqlib.postprocessing.ProteinAbundanceInference;
 
 /**
- * 
+ *
  * @author SPerkins
  */
 public class ProteinInferenceHelper {
-	public static final String REPORTER_ION_INTENSITY = "MS:1001847";
-	public static final String LCMS_FEATURE_INTENSITY = "MS:1001840";
-	public static final String LABEL_FREE_PEPTIDE = "MS:1001891";
-	private static final String LABEL_FREE_PROTEIN = "MS:1001890";
-	private static final String LABEL_FREE_PEPTIDE_DESC = "Progenesis: peptide normalised abundance";
-	private static final String LABEL_FREE_PROTEIN_DESC = "Progenesis: protein normalised abundance";
-	private static final String REPORTER_ION_INTENSITY_DESC = "reporter ion intensity";
-	private static final String LCMS_FEATURE_INTENSITY_DESC = "LC-MS feature intensity";
-	private static final String quantLayerType = "AssayQuantLayer";
-	private static String outputFile = null;
 
-	private ProteinInferenceHelper() {
-	}
+    public static final String REPORTER_ION_INTENSITY = "MS:1001847";
+    public static final String LCMS_FEATURE_INTENSITY = "MS:1001840";
+    public static final String LABEL_FREE_PEPTIDE = "MS:1001891";
+    private static final String LABEL_FREE_PROTEIN = "MS:1001890";
+    private static final String LABEL_FREE_PEPTIDE_DESC = "Progenesis: peptide normalised abundance";
+    private static final String LABEL_FREE_PROTEIN_DESC = "Progenesis: protein normalised abundance";
+    private static final String REPORTER_ION_INTENSITY_DESC = "reporter ion intensity";
+    private static final String LCMS_FEATURE_INTENSITY_DESC = "LC-MS feature intensity";
+    private static final String quantLayerType = "AssayQuantLayer";
+    private static String outputFile = null;
 
-	public static void infer(final String inputFile, final String quantMethod,
-			final String quantDataType, final String mergeOperator) {
-		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+    private ProteinInferenceHelper() {
+    }
 
-			@Override
-			public Void doInBackground() {
-				AnalyseDynamicTab.getInstance().getAnalyseStatusPanel()
-						.setProteinInferenceProcessing();
-				AnalyseData
-						.getInstance()
-						.getTasksModel()
-						.set(new Task(FileUtils
-								.getFileNameFromFullPath(inputFile),
-								"Inferring Proteins"));
-				TasksTab.getInstance().refreshFromTasksModel();
-				outputFile = inputFile.replaceAll(".mzq",
-						"_protein_inference.mzq");
-				try {
-					if (quantDataType.equals(REPORTER_ION_INTENSITY)) {
-						new ProteinAbundanceInference(inputFile, outputFile,
-								quantMethod, mergeOperator, quantDataType,
-								LABEL_FREE_PROTEIN, LABEL_FREE_PROTEIN_DESC,
-								quantLayerType);
-					} else if (quantDataType.equals(LCMS_FEATURE_INTENSITY)) {
-						new ProteinAbundanceInference(inputFile, outputFile,
-								quantMethod, mergeOperator, quantDataType,
-								LABEL_FREE_PROTEIN, LABEL_FREE_PROTEIN_DESC,
-								quantLayerType);
-					}
-				} catch (FileNotFoundException ex) {
-					System.out.println("Protein inference error: "
-							+ ex.getLocalizedMessage());
-				}
+    public static void infer(final String inputFile, final String quantMethod,
+            final String quantDataType, final String mergeOperator) {
+        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
 
-				return null;
-			}
+            @Override
+            public Void doInBackground() {
+                AnalyseDynamicTab.getInstance().getAnalyseStatusPanel()
+                        .setProteinInferenceProcessing();
+                AnalyseData
+                        .getInstance()
+                        .getTasksModel()
+                        .set(new Task(FileUtils
+                                        .getFileNameFromFullPath(inputFile),
+                                        "Inferring Proteins"));
+                TasksTab.getInstance().refreshFromTasksModel();
+                outputFile = inputFile.replaceAll(".mzq",
+                        "_protein_inference.mzq");
+                try {
+                    if (quantDataType.equals(REPORTER_ION_INTENSITY)) {
+                        new ProteinAbundanceInference(inputFile, outputFile,
+                                quantMethod, mergeOperator, quantDataType,
+                                LABEL_FREE_PROTEIN, LABEL_FREE_PROTEIN_DESC,
+                                quantLayerType);
+                    } else if (quantDataType.equals(LCMS_FEATURE_INTENSITY)) {
+                        new ProteinAbundanceInference(inputFile, outputFile,
+                                quantMethod, mergeOperator, quantDataType,
+                                LABEL_FREE_PROTEIN, LABEL_FREE_PROTEIN_DESC,
+                                quantLayerType);
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println("Protein inference error: "
+                            + ex.getLocalizedMessage());
+                }
 
-			@Override
-			public void done() {
-				try {
-					get();
+                return null;
+            }
 
-					AnalyseDynamicTab.getInstance().getAnalyseStatusPanel()
-							.setProteinInferenceDone();
-					AnalyseData
-							.getInstance()
-							.getTasksModel()
-							.set(new Task(FileUtils
-									.getFileNameFromFullPath(inputFile),
-									"Inferring Proteins", "Complete"));
-					TasksTab.getInstance().refreshFromTasksModel();
+            @Override
+            public void done() {
+                try {
+                    get();
 
-					AnalyseData
-							.getInstance()
-							.getInspectModel()
-							.addQuantDataFile(
-									new MzQuantMLFile(new File(outputFile)));
-				} catch (InterruptedException | ExecutionException ex) {
-					System.out.println(ex.getLocalizedMessage());
-				}
+                    AnalyseDynamicTab.getInstance().getAnalyseStatusPanel()
+                            .setProteinInferenceDone();
+                    AnalyseData
+                            .getInstance()
+                            .getTasksModel()
+                            .set(new Task(FileUtils
+                                            .getFileNameFromFullPath(inputFile),
+                                            "Inferring Proteins", "Complete"));
+                    TasksTab.getInstance().refreshFromTasksModel();
 
-			}
-		};
+                    AnalyseData
+                            .getInstance()
+                            .getInspectModel()
+                            .addQuantDataFile(
+                                    new MzQuantMLFile(new File(outputFile)));
+                } catch (InterruptedException | ExecutionException ex) {
+                    System.out.println(ex.getLocalizedMessage());
+                }
 
-		AnalyseData.getInstance().getExecutor().submit(worker);
-	}
+            }
+        };
+
+        AnalyseData.getInstance().getExecutor().submit(worker);
+    }
 }
