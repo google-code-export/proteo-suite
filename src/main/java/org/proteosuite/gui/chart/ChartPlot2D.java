eca579c798910c4b6923f213647bf7c3f4d00ba3
detritus.c@gmail.com <detritus.c@gmail.com@a6fb7cc7-c7c0-847f-fedf-08a8b6925e11>
2014-05-15 15:43:26 +0000

    * Removed 2D Chart from having its own threshold controls * Added threshold control for raw data, affecting both chart and table * Removed caching as performance no longer requires it * Removed excessive casting from table construction

--- src/main/java/org/proteosuite/gui/chart/ChartPlot2D.java
+++ src/main/java/org/proteosuite/gui/chart/ChartPlot2D.java
@@ -1,21 +1,11 @@
 package org.proteosuite.gui.chart;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Float;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-import org.proteosuite.model.RawMzMLFile;
 import org.proteosuite.utils.TwoDPlot;
 
 import uk.ac.ebi.jmzml.model.mzml.BinaryDataArray;
@@ -28,17 +18,16 @@ import uk.ac.ebi.jmzml.xml.io.MzMLUnmarshaller;
  * @author Andrew Collins
  */
 public class ChartPlot2D extends AbstractChart {
-	private static final int CHART_INDEX = 1;
-	private static double LOW_INTENSITY = 10;
-	private static byte MS_LEVEL = 1;
 
 	/**
 	 * Displays the MS1 raw data as 2D plots
 	 * 
 	 * @param unmarshaller
+	 * @param lowIntensity 
+	 * @param msLevel 
 	 * @return
 	 */
-	public static JPanel get2DPlot(MzMLUnmarshaller unmarshaller) {
+	public static JPanel get2DPlot(MzMLUnmarshaller unmarshaller, byte msLevelThreshold, double lowIntensityThreshold) {
 
 		// Check if mzML contains MS1 data
 		if (unmarshaller.getChromatogramIDs().isEmpty())
@@ -55,7 +44,7 @@ public class ChartPlot2D extends AbstractChart {
 			// Identify MS1 data
 			byte msLevel = getMSLevel(spectrum.getCvParam());
 
-			if (msLevel != MS_LEVEL)
+			if (msLevel != msLevelThreshold)
 				continue;
 
 			float rt = getRetentionTime(spectrum.getScanList().getScan().get(0)
@@ -74,7 +63,7 @@ public class ChartPlot2D extends AbstractChart {
 					intensities = getDouble(getIntensity(binaryData));
 			}
 
-			addPoints(points, rt, massCharges, intensities);
+			addPoints(points, rt, massCharges, intensities, lowIntensityThreshold);
 		}
 
 		float[][] data = convertPoints(points);
@@ -95,66 +84,13 @@ public class ChartPlot2D extends AbstractChart {
 	}
 
 	private static void addPoints(final Set<Float> points, float rt,
-			float[] massCharges, double[] intensities) {
+			float[] massCharges, double[] intensities, double lowIntensityThreshold) {
 		for (int i = 0; i < massCharges.length; i++) {
 			// Removing low values
-			if (intensities[i] <= LOW_INTENSITY)
+			if (intensities[i] <= lowIntensityThreshold)
 				continue;
 
 			points.add(new Point2D.Float(massCharges[i], rt));
 		}
-	}
-
-	public static JPanel get2DPlot(RawMzMLFile dataFile) {
-		JPanel fullPanel = new JPanel(new BorderLayout());
-		fullPanel.add(getButtonPanel(fullPanel, dataFile),
-				BorderLayout.PAGE_END, 0);
-		fullPanel.add(get2DPlot(dataFile.getUnmarshaller()),
-				BorderLayout.CENTER, CHART_INDEX);
-
-		return fullPanel;
-	}
-
-	// TODO: Make this a bit more sane
-	private static JPanel getButtonPanel(final JPanel fullPanel, final RawMzMLFile dataFile) {
-		JPanel panel = new JPanel(new FlowLayout());
-
-		panel.add(new JLabel("MS Level:"));
-		final JComboBox<Integer> msLevel = new JComboBox<>();
-		msLevel.addItem(1);
-		msLevel.addItem(2);
-		msLevel.setSelectedIndex(MS_LEVEL - 1);
-
-		panel.add(msLevel);
-
-		final JTextField threshold = new JTextField(String.valueOf(LOW_INTENSITY), 5);
-
-		panel.add(new JLabel("Minimum Intensity:"));
-		panel.add(threshold);
-
-		JButton refresh = new JButton("Refresh");
-		refresh.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					ChartPlot2D.LOW_INTENSITY = Double.parseDouble(threshold
-							.getText());
-				} catch (NumberFormatException exception) {
-					// User entered non-numeric data
-					threshold.setText(String.valueOf(ChartPlot2D.LOW_INTENSITY));
-				}
-				ChartPlot2D.MS_LEVEL = Byte.parseByte(msLevel.getItemAt(
-						msLevel.getSelectedIndex()).toString());
-				fullPanel.remove(CHART_INDEX);
-				fullPanel.add(
-						ChartPlot2D.get2DPlot(dataFile.getUnmarshaller()),
-						BorderLayout.CENTER, CHART_INDEX);
-				fullPanel.revalidate();
-			}
-		});
-                
-		panel.add(refresh);
-
-		return panel;
 	}
 }
--- src/main/java/org/proteosuite/gui/inspect/InspectRaw.java
+++ src/main/java/org/proteosuite/gui/inspect/InspectRaw.java
@@ -1,36 +1,91 @@
 package org.proteosuite.gui.inspect;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import org.proteosuite.gui.chart.ChartChromatogram;
+import org.proteosuite.gui.chart.ChartPlot2D;
 import org.proteosuite.gui.chart.ChartSpectrum;
+import org.proteosuite.gui.tables.JTableMzML;
 import org.proteosuite.model.AnalyseData;
 import org.proteosuite.model.InspectModel;
 import org.proteosuite.model.RawMzMLFile;
 
+import com.compomics.util.gui.spectrum.ChromatogramPanel;
 import com.compomics.util.gui.spectrum.SpectrumPanel;
 
 public class InspectRaw extends JPanel implements ListSelectionListener {
 	private static final long serialVersionUID = 1L;
+	private static byte MS_LEVEL = 1;
+	private static double LOW_INTENSITY = 10;
 	private final InspectChartPanel chartPanel = new InspectChartPanel();
 	private final JTable jTable = new JTable();
 	private final InspectModel inspectModel = AnalyseData.getInstance()
 			.getInspectModel();
+	private RawMzMLFile data;
 
 	public InspectRaw() {
-		super(new GridLayout(1, 2));
-		add(chartPanel);
-		add(new JScrollPane(jTable));
+		super(new BorderLayout());
+		JPanel mainContent = new JPanel(new GridLayout(1, 2));
+		add(getButtonPanel(), BorderLayout.PAGE_END);
+		add(mainContent, BorderLayout.CENTER);
+		mainContent.add(chartPanel);
+		mainContent.add(new JScrollPane(jTable));
 
 		jTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		jTable.getSelectionModel().addListSelectionListener(this);
 		jTable.setAutoCreateRowSorter(true);
+	}
+	
+	private JPanel getButtonPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+
+		panel.add(new JLabel("MS Level:"));
+		final JComboBox<Integer> msLevel = new JComboBox<>();
+		msLevel.addItem(1);
+		msLevel.addItem(2);
+		msLevel.setSelectedIndex(MS_LEVEL - 1);
+
+		panel.add(msLevel);
+
+		final JTextField threshold = new JTextField(Double.toString(LOW_INTENSITY), 5);
+
+		panel.add(new JLabel("Minimum Intensity:"));
+		panel.add(threshold);
+
+		JButton refresh = new JButton("Refresh");
+		refresh.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MS_LEVEL = Byte.parseByte(msLevel.getItemAt(
+						msLevel.getSelectedIndex()).toString());
+				try {
+					LOW_INTENSITY = Double.parseDouble(threshold
+							.getText());
+					setData(data);
+				} catch (NumberFormatException exception) {
+					// User entered non-numeric data
+					threshold.setText(String.valueOf(LOW_INTENSITY));
+				}
+			}
+		});
+		panel.add(refresh);
+
+		return panel;
 	}
 
 	public JTable getTablePanel() {
@@ -63,5 +118,26 @@ public class InspectRaw extends JPanel implements ListSelectionListener {
 					.getSpectrum(rawMzMLFile.getUnmarshaller(), sID);
 			chartPanel.setSpectrum(specPanel);
 		}
+	}
+
+	public void setData(RawMzMLFile dataFile) {
+		data = dataFile;
+		
+		JTableMzML rawData = new JTableMzML();
+		rawData.showData(dataFile, MS_LEVEL, LOW_INTENSITY);
+
+		JTable jTable = getTablePanel();
+		jTable.removeAll();
+		jTable.setModel(rawData.getModel());
+
+		rawData.getSelectionModel().addListSelectionListener(this);
+		ChromatogramPanel cachedChromatogram = ChartChromatogram.getChromatogram(dataFile
+					.getUnmarshaller());
+
+		getChartPanel().setChromatogram(cachedChromatogram);
+
+		JPanel cached2DView = ChartPlot2D.get2DPlot(dataFile.getUnmarshaller(), MS_LEVEL, LOW_INTENSITY);
+
+		getChartPanel().set2D(cached2DView);
 	}
 }
